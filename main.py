2749b65982642d1a202c8fbb67ec7019f0a26f75
Jeet Majumdar <bonimajumdar2003@gmail.com>
2025-09-16 01:11:08 +0530

    Stage-4 Implemented Training stage with configuration, model training, and callback handling

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from cnnClassifier.pipeline.stage_03_training import TrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -24,6 +25,20 @@ if __name__ == "__main__":
         logger.info(f"***********************************")
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         pipeline = PrepareBaseModelTrainingPipeline()
+        pipeline.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(f"Error in stage {STAGE_NAME}: {e}")
+        raise e
+    
+
+
+STAGE_NAME = "Training Stage"
+if __name__ == "__main__":
+    try:
+        logger.info(f"***********************************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        pipeline = TrainingPipeline()
         pipeline.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
